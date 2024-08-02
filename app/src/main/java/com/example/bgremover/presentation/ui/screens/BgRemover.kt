91fdd55dc29a9cde36d5e6b9c30d34427dfab3c8
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-08-02 11:13:18 +0500

    ui change

--- app/src/main/java/com/example/bgremover/presentation/ui/screens/BgRemover.kt
+++ app/src/main/java/com/example/bgremover/presentation/ui/screens/BgRemover.kt
@@ -17,14 +17,21 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -34,9 +41,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.NotificationCompat
 import coil.compose.rememberImagePainter
 import com.example.bgremover.R
@@ -46,6 +58,8 @@ import com.example.bgremover.presentation.viewmodel.MainViewModel
 import org.koin.compose.koinInject
 import java.io.File
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BgRemover() {
     val viewModel: MainViewModel = koinInject()
@@ -91,59 +105,54 @@ fun BgRemover() {
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        imageUri?.let {
-            val painter = rememberImagePainter(it)
-            Image(painter = painter, contentDescription = null, modifier = Modifier.size(200.dp))
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { launcher.launch("image/*") }) {
-            Text("Select Image")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            imageFile?.let { file ->
-                isLoading = true
-                viewModel.removeBackground(file)
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Row {
+                Text(
+                    text = "remover",
+                    color = Color(0XFF454545),
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 27.sp
+                )
+                Text(
+                    text = "bg",
+                    color = Color(0XFFbbc1c5),
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Medium
+                )
             }
-        }) {
-            Text("Remove Background")
-        }
+        }, navigationIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.bgremover),
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .size(60.dp),
+            )
+        }, actions = {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "",
+                modifier = Modifier.height(39.dp)
+            )
+        })
+    }) {
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding()),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        if (isLoading) {
-            CircularProgressIndicator()
-        } else {
-            bgRemovedImageBase64?.let { base64 ->
-                val imageBytes = Base64.decode(base64, Base64.DEFAULT)
-                val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-                Column {
-                    Image(
-                        bitmap = bitmap.asImageBitmap(),
-                        contentDescription = null,
-                        modifier = Modifier.size(200.dp)
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        saveImage(bitmap, context)
-                    }) {
-                        Text("Download Image")
-                    }
-                }
-            }
+            Spacer(modifier = Modifier.height(20.dp))
+            
+            Image(painter = painterResource(id = R.drawable.target), contentDescription = "")
         }
     }
+
 }
 
 
