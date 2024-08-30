21b3df82ca4e1c3991fbabfbc8cc6afaa89681c5
Muhammad Zohaib Imtiaz <159874349+ZohaibKhanDev@users.noreply.github.com>
2024-08-30 06:03:03 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bgremover"
-        minSdk = 24
+        minSdk = 24 
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
